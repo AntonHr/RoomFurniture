728e30942b5b545e8583e2a7fc8aec5ea28ce7cf
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-12-15 09:27:47 +0000

    refactored algorithm structure

--- desktop/src/com/awesome/scenario/desktop/DesktopLauncher.java
+++ desktop/src/com/awesome/scenario/desktop/DesktopLauncher.java
@@ -1,10 +1,7 @@
 package com.awesome.scenario.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gui.EvaluatorPhysicsRenderer;
-import com.gui.SwingVisualizer;
-import com.roomfurniture.InputParser;
 import com.roomfurniture.ShapeCalculator;
 import com.roomfurniture.angle.*;
 import com.roomfurniture.problem.Descriptor;
@@ -12,13 +9,8 @@ import com.roomfurniture.problem.Furniture;
 import com.roomfurniture.problem.Problem;
 import com.roomfurniture.problem.Vertex;
 import com.roomfurniture.solution.Solution;
-import com.roomfurniture.solution.storage.SolutionDatabase;
 
-import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 public class DesktopLauncher {
 
@@ -30,174 +22,12 @@ public class DesktopLauncher {
 //        PhysicsApproach.runMultiThreaded();
 //        PhysicsApproach.runPhysicsSingleThreaded();
 
-//        PhysicsApproach.runSpawnPointPicker();
-
-
-        Problem problem = InputParser.getTestProblems().get(0);
-//        List<AngleSet> angleSets = AngleSet.generateInitialAngleSet(problem);
-//        AngleSet angleSet = angleSets.get(1);
-//        List<List<FurnitureAngleSet>> solutions = angleSet.getSolutions();
-//        List<FurnitureAngleSet> furnitureAngleSets = solutions.get(0);
-//        Angle angle = furnitureAngleSets.get(1).getAngles().get(0);
-//        Angle stationary = angleSet.getAngles().get(0);
-//        System.out.println("Align Stationary " + stationary +" and " + angle);
-//        Descriptor descriptor = EdgeAligner.alignTwoAngles(stationary, angle);
-//
-//        Furniture furniture = problem.getFurnitures().get(1);
-
-        List<Furniture> furnitures = new ArrayList<>();
-        List<Furniture> collect = problem.getFurnitures().stream().sorted(Comparator.comparingDouble(Furniture::getScore)).collect(Collectors.toList());
-        Collections.reverse(collect);
-        Problem testProblem = new Problem(problem.getNumber(), problem.getRoom(), collect);
-        Furniture furniture = collect.get(4);
-//k        Furniture furniture = problem.getFurnitures().get(2);
-        furnitures.add(furniture);
-//        Problem testProblem =  new Problem(1, problem.getRoom(), furnitures);
-//
-//        System.out.println(descriptor);
-//        List<AngleSet> angleSets = AngleSet.generateInitialAngleSet(problem);
-//
-//        List<AngleSet> angleSets1 = foldAngleSets(angleSets);
-//
-//        // TODO: Change to only list valid angle sets
-//        List<AngleSet> matchingAngleSets = geMatchingAngleSets(furniture, angleSets1);
-//
-//        for(AngleSet set : matchingAngleSets) {
-//            System.out.println(set.getAngles() + " are a valid candidate for this furniture" + set.getSolutions().get(furniture));
-//        }
-//        List<FurnitureAngleSet> furnitureAngleSets = matchingAngleSets.get(0).getSolutions().get(furniture);
-//        List<Angle> anglesonRoom = matchingAngleSets.get(1).getAngles();
-//        List<Angle> anglesonShape = furnitureAngleSets.get(1).getAngles();
-//
-//        System.out.println("Angles on room: " + anglesonRoom);
-//        System.out.println("Angles on shap: " + anglesonShape);
-//        List<Angle> angles1 = EdgeAligner.computeShapeAngles(furniture.getVertices());
-//        List<Angle> angles = EdgeAligner.computeShapeAngles(problem.getRoom().getVerticies());
-//
-//        Angle angle = angles.get(4);
-//        for(int i = 0; i < angles1.size(); i++) {
-//            System.out.println("[" + i + "]:  " +  angles.get(i));
-//        }
-//
-//        Angle angleRoom = anglesonRoom.get(1);
-//        Angle angleShape = anglesonRoom.get(1);
-//
-//        System.out.println(angle);
-//        for(int i = 0; i < angles1.size(); i++) {
-//            System.out.println("[" + i + "]:  " +  angles1.get(i));
-//        }
-//        Angle angle1 = angles1.get(0);
-//        System.out.println(angle1);
-////        Descriptor descriptor = EdgeAligner.alignTwoAngles(angle, angle1);
-//        Descriptor descriptor = EdgeAligner.alignTwoAngles(angleRoom, angleShape);
-//        System.out.println(descriptor);
-//
-//        Solution testSolution =  new Solution(Arrays.asList(
-////               descriptor
-//                new Descriptor(new Vertex(0,0),0)
-//        ));
-
-        Solution testSolution = solveProblem(testProblem);
-
-
-        ExecutorService service = Executors.newFixedThreadPool(10);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, true);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, false);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, true);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, false);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, true);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, false);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, true);
-        testSolution = BasicApproach.optimizeSolution(testSolution, testProblem, service, 1000, 1000, 10, false);
-
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 //        DragAndDrop renderer = new DragAndDrop(problem, new DragAndDropPhysicsSimulator(problem.getRoom()));
+        PhysicsApproach.runSpawnPointPicker();
 
-        EvaluatorPhysicsRenderer renderer = new EvaluatorPhysicsRenderer();
 
-        try {
-            SwingVisualizer.visualizeProblem(testProblem, testSolution);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        config.width = 2000;
-        config.height = 1000;
-
-        DesktopLauncher.application = new LwjglApplication(renderer, config);
-        renderer.renderSolution(testProblem,testSolution );
-
-         if (testProblem.getNumber() > 0 && testProblem.getNumber() <= 30) {
-            SolutionDatabase.createTeamSolutionDatabase().storeSolutionFor(testProblem.getNumber(), testSolution.score(testProblem).get(),testSolution.findCoverage(testProblem), testSolution);
-        }
+//        AlgorithmApproach.runAlgorithmicSolver();
     }
-
-    public static List<AngleSet> geMatchingAngleSets(Furniture furniture, List<AngleSet> angleSets1) {
-        List<AngleSet> matchingAngleSets = new ArrayList<>();
-        for(AngleSet set : angleSets1) {
-            if(set.getSolutions().get(furniture).size() != 0) {
-                matchingAngleSets.add(set);
-            }
-        }
-        return matchingAngleSets;
-    }
-
-    public static Solution solveProblem(Problem problem) {
-        List<AngleSet> angleSets = AngleSet.generateInitialAngleSet(problem);
-        angleSets = foldAngleSets(angleSets);
-        angleSets = foldAngleSets(angleSets);
-        angleSets = foldAngleSets(angleSets);
-        List<Descriptor> descriptors = solveFurniturePositioningFor(problem, 0, angleSets);
-        return new Solution(descriptors);
-
-    }
-
-    public final static List<Descriptor> solveFurniturePositioningFor(Problem problem, int index, List<AngleSet> angleSets) {
-        if(index >= problem.getFurnitures().size())
-            return new ArrayList<>();
-        else if(index >= 200) {
-            ArrayList<Descriptor> descriptors = new ArrayList<>();
-            for(int i = index; i < problem.getFurnitures().size(); i++) {
-                descriptors.add(new Descriptor(new Vertex(0,0),0));
-            }
-            return descriptors;
-        } else {
-                Furniture furniture = problem.getFurnitures().get(index);
-                List<AngleSet> furnitureAngleSets = geMatchingAngleSets(furniture, angleSets);
-                for(AngleSet set : furnitureAngleSets) {
-                    List<Angle> angles = set.getAngles();
-                    List<FurnitureAngleSet> furnitureAngles = set.getSolutions().get(furniture);
-                    for(FurnitureAngleSet solutionSet : furnitureAngles) {
-                        List<Angle> solutionAngles = solutionSet.getAngles();
-                        for (int i = 0; i < angles.size(); i++) {
-                            Descriptor descriptor = EdgeAligner.alignTwoAngles(angles.get(i), solutionAngles.get(i));
-                            Furniture possible = furniture.transform(descriptor);
-                            if(ShapeCalculator.contains(problem.getRoom().toShape(), possible.toShape())) {
-                                System.out.println("Solution found");
-                                // TODO Check if recursive branch creates conflicts
-                                List<Descriptor> result = solveFurniturePositioningFor(problem, index + 1, angleSets);
-                                result.add(0, descriptor);
-                                return result;
-                            }
-                        }
-                    }
-                }
-
-                System.out.println("No solution found");
-                List<Descriptor> result = solveFurniturePositioningFor(problem, index + 1, angleSets);
-                result.add(0, new Descriptor(new Vertex(0,0),0));
-                return result;
-            }
-        }
-
-    public static List<AngleSet> foldAngleSets(List<AngleSet> angleSets) {
-        List<AngleSet> conjoined = new ArrayList<>();
-        for(int i = 0; i < angleSets.size()-1; i++) {
-                AngleSet.conjoinAngleSets(angleSets.get(i), angleSets.get(i+1)).ifPresent(conjoined::add);
-        }
-        return conjoined;
-    }
-
 
 
 }
